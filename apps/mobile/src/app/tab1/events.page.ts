365025a772e023a62c0b8a7714139285d761e504
PancakeMash <arpandevofficial@gmail.com>
2026-02-26 21:51:13 +0000

    JA-4 Generic button WIP

--- apps/mobile/src/app/tab1/events.page.ts
+++ apps/mobile/src/app/tab1/events.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonLabel, IonItem } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonLabel, IonItem, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
 import { Event } from '../../apptyping';
 
 @Component({
   selector: 'app-events',
   templateUrl: 'events.page.html',
   styleUrls: ['events.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonFab, IonFabButton, IonList, IonLabel, IonIcon],
 })
 
 
