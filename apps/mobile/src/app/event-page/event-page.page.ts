dcb130b535b9b72fbbe1365a8eb6a5f942212b94
PancakeMash <arpandevofficial@gmail.com>
2026-03-01 21:23:10 +0000

    JA-4 Fixing buttons with refs so that the entire button can be clicked

--- apps/mobile/src/app/event-page/event-page.page.ts
+++ apps/mobile/src/app/event-page/event-page.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './event-page.page.html',
   styleUrls: ['./event-page.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, IonButton],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton],
 })
 export class EventPagePage implements OnInit {
 
